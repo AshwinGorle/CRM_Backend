6ef952381cee1f6b8b12516112a50a8d1c036e7e
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-08-24 22:17:49 +0530

    origin fixed

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ import authenticateToken from '../middlewares/authenticateToken.js';
 const app = express();
 dotenv.config();
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials : true
 }
 
