87a4d3120c21cfc94d4eaab98480f469169bc2ed
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-08-27 17:12:18 +0530

    cors test

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ import authenticateToken from '../middlewares/authenticateToken.js';
 const app = express();
 dotenv.config();
 const corsOptions = {
-    origin: "*",
+    origin: ["http://localhost:3000", "https://crm-frontend-sigma-green.vercel.app/"],
     credentials : true
 }
 
