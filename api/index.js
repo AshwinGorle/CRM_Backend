82ada28428c2e2e6991765a2175c2e00ad153cf6
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-10-26 18:52:04 +0530

    db url 2

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
-const DB_URL = process.env.DATABASE_URL; 
+const DB_URL = process.env.DATABASE2_URL; 
 connectDb(DB_URL);
 
 app.get('/',homePage);
